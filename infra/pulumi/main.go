df3de396f1d41e7b211531a16f96df05ceecfd8b
Johannes Rosskopp <johannes.rosskopp@prodyna.com>
2025-02-23 17:54:43 +0000

    Ading MySql deployment

--- infra/pulumi/main.go
+++ infra/pulumi/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/pulumi/pulumi-azure-native-sdk/dbformysql/v2"
 	"github.com/pulumi/pulumi-azure-native-sdk/network/v2"
 	"github.com/pulumi/pulumi-azure-native-sdk/resources/v2"
 	"github.com/pulumi/pulumi-azure-native-sdk/sql/v2"
@@ -28,90 +29,205 @@ func main() {
 			return err
 		}
 
-		// Create a Subnet
-		subnet, err := network.NewSubnet(ctx, "foxnhound-subnet", &network.SubnetArgs{
-			ResourceGroupName:  resourceGroup.Name,
-			VirtualNetworkName: vnet.Name,
-			AddressPrefix:      pulumi.String("10.0.1.0/24"),
-		})
-		if err != nil {
-			return err
-		}
+		// Create a SQL Server
+		// _, err = createSqlServer(ctx, resourceGroup, subnet)
+		// if err != nil {
+		// 	return err
+		// }
 
-		dbserver, err := sql.NewServer(ctx, "foxnhound-db-server", &sql.ServerArgs{
-			Administrators: &sql.ServerExternalAdministratorArgs{
-				AzureADOnlyAuthentication: pulumi.Bool(true),
-				Login:                     pulumi.String("j.rosskopp@gmx.de"),
-				PrincipalType:             pulumi.String(sql.PrincipalTypeUser),
-				Sid:                       pulumi.String("1685f20c-0f20-4999-aaa0-550994bcc380"),
-				TenantId:                  pulumi.String("e467b6d8-cf62-4e59-9a87-758ef858aeb6"),
-			},
-			PublicNetworkAccess:           pulumi.String(sql.ServerNetworkAccessFlagDisabled),
-			ResourceGroupName:             resourceGroup.Name,
-			RestrictOutboundNetworkAccess: pulumi.String(sql.ServerNetworkAccessFlagDisabled),
-		})
-		if err != nil {
-			return err
-		}
-
-		_, err = sql.NewDatabase(ctx, "foxnhound-db", &sql.DatabaseArgs{
-			ResourceGroupName: resourceGroup.Name,
-			ServerName:        dbserver.Name,
-			Sku: &sql.SkuArgs{
-				Name:     pulumi.String("Basic"),
-				Tier:     pulumi.String("Basic"),
-			},
-		})
-		if err != nil {
-			return err
-		}
-
-		// Create a Private Endpoint for the Azure SQL Database
-		privateEndpoint, err := network.NewPrivateEndpoint(ctx, "foxnhound-private-endpoint", &network.PrivateEndpointArgs{
-			ResourceGroupName: resourceGroup.Name,
-			Subnet: &network.SubnetTypeArgs{
-				Id: subnet.ID(),
-			},
-			PrivateLinkServiceConnections: network.PrivateLinkServiceConnectionArray{
-				&network.PrivateLinkServiceConnectionArgs{
-					Name:                 pulumi.String("sqlPrivateLink"),
-					PrivateLinkServiceId: dbserver.ID(),
-					GroupIds: pulumi.StringArray{
-						pulumi.String("sqlServer"),
-					},
-					RequestMessage: pulumi.String("Please approve my connection"),
-				},
-			},
-		})
-		if err != nil {
-			return err
-		}
-
-		// Create a Private DNS Zone for the SQL Server
-		privateDnsZone, err := network.NewPrivateZone(ctx, "foxnhound-private-dns-zone", &network.PrivateZoneArgs{
-			ResourceGroupName: resourceGroup.Name,
-			PrivateZoneName:  pulumi.String("foxnhound.privatelink.database.windows.net"),
-			Location:          pulumi.String("Global"),
-		})
-		if err != nil {
-			return err
-		}
-
-		// Create a DNS Zone Group for the Private Endpoint
-		_, err = network.NewPrivateDnsZoneGroup(ctx, "foxnhound-dns-zone-group", &network.PrivateDnsZoneGroupArgs{
-			ResourceGroupName:   resourceGroup.Name,
-			PrivateEndpointName: privateEndpoint.Name,
-			PrivateDnsZoneConfigs: network.PrivateDnsZoneConfigArray{
-				&network.PrivateDnsZoneConfigArgs{
-					Name:             pulumi.String("sqlDnsZoneConfig"),
-					PrivateDnsZoneId: privateDnsZone.ID(),
-				},
-			},
-		})
-		if err != nil {
-			return err
+		// Create MySQL Server
+		returnArgs := createMySqlServer(MySqlServerArgs{
+			ctx:           ctx,
+			resourceGroup: resourceGroup,
+			vnet:          vnet})
+		if returnArgs.err != nil {
+			return returnArgs.err
 		}
 
 		return nil
 	})
+
+}
+
+type MySqlServerArgs struct {
+	ctx           *pulumi.Context
+	resourceGroup *resources.ResourceGroup
+	vnet          *network.VirtualNetwork
+}
+
+type MySqlServerReturn struct {
+	dbserver *dbformysql.Server
+	err      error
+}
+
+func createMySqlServer(args MySqlServerArgs) MySqlServerReturn {
+
+	// Cretae a new delegated subnet
+	subnet, err := network.NewSubnet(args.ctx, "delegatedSubnet", &network.SubnetArgs{
+		Delegations: network.DelegationArray{
+			&network.DelegationArgs{
+				ServiceName: pulumi.String("Microsoft.DBforMySQL/flexibleServers"),
+				Name:        pulumi.String("mysqlDelegation"),
+			},
+		},
+		ResourceGroupName:  args.resourceGroup.Name,
+		VirtualNetworkName: args.vnet.Name,
+		AddressPrefix:      pulumi.String("10.0.1.0/24"),
+	})
+	if err != nil {
+		return MySqlServerReturn{err: err}
+	}
+
+	dnszone, err := network.NewPrivateZone(args.ctx, "foxnhound-private-dns-zone", &network.PrivateZoneArgs{
+		ResourceGroupName: args.resourceGroup.Name,
+		PrivateZoneName:   pulumi.String("foxnhound.mysql.database.azure.com"),
+		Location:          pulumi.String("Global"),
+	})
+	if err != nil {
+		return MySqlServerReturn{err: err}
+	}
+
+	networklink, err := network.NewVirtualNetworkLink(args.ctx, "foxnhound-db-nwlink", &network.VirtualNetworkLinkArgs{
+		ResourceGroupName:   args.resourceGroup.Name,
+		PrivateZoneName:     dnszone.Name,
+		Location:            pulumi.String("Global"),
+		RegistrationEnabled: pulumi.Bool(false),
+		VirtualNetwork: &network.SubResourceArgs{
+			Id: args.vnet.ID(),
+		},
+	})
+	if err != nil {
+		return MySqlServerReturn{err: err}
+	}
+
+	dbserver, err := dbformysql.NewServer(args.ctx, "foxnhound-mysql-server", &dbformysql.ServerArgs{
+		AdministratorLogin:         pulumi.String("sqladmin_jH5JKsj_54KJH"),
+		AdministratorLoginPassword: pulumi.String("jHGJ7JKsd(sjd)jkh%"),
+		ResourceGroupName:          args.resourceGroup.Name,
+		Sku: &dbformysql.SkuArgs{
+			Name: pulumi.String("Standard_B1ms"),
+			Tier: pulumi.String(dbformysql.SkuTierBurstable),
+		},
+		Version: pulumi.String(dbformysql.ServerVersion_8_0_21),
+		Network: &dbformysql.NetworkArgs{
+			DelegatedSubnetResourceId: subnet.ID(),
+			PrivateDnsZoneResourceId:  dnszone.ID(),
+		},
+	},
+		pulumi.DependsOn([]pulumi.Resource{subnet, dnszone, networklink}))
+	if err != nil {
+		return MySqlServerReturn{err: err}
+	}
+
+	_, err = dbformysql.NewDatabase(args.ctx, "foxnhound-db", &dbformysql.DatabaseArgs{
+		Charset:           pulumi.String("utf8"),
+		Collation:         pulumi.String("utf8_general_ci"),
+		ServerName:        dbserver.Name,
+		DatabaseName:      dbserver.Name,
+		ResourceGroupName: args.resourceGroup.Name,
+	})
+	if err != nil {
+		return MySqlServerReturn{err: err}
+	}
+
+	return MySqlServerReturn{dbserver: dbserver}
+
+}
+
+type SqlServerArgs struct {
+	ctx           *pulumi.Context
+	resourceGroup *resources.ResourceGroup
+	vnet          *network.VirtualNetwork
+}
+
+type SqlServerReturn struct {
+	dbserver *sql.Server
+	err      error
+}
+
+func createSqlServer(args SqlServerArgs) SqlServerReturn {
+	subnet, err := network.NewSubnet(args.ctx, "foxnhound-subnet", &network.SubnetArgs{
+		ResourceGroupName:  args.resourceGroup.Name,
+		VirtualNetworkName: args.vnet.Name,
+		AddressPrefix:      pulumi.String("10.0.1.0/24"),
+	})
+	if err != nil {
+		return SqlServerReturn{err: err}
+	}
+
+	dbserver, err := sql.NewServer(args.ctx, "foxnhound-db-server", &sql.ServerArgs{
+		Administrators: &sql.ServerExternalAdministratorArgs{
+			AzureADOnlyAuthentication: pulumi.Bool(true),
+			Login:                     pulumi.String("j.rosskopp@gmx.de"),
+			PrincipalType:             pulumi.String(sql.PrincipalTypeUser),
+			Sid:                       pulumi.String("1685f20c-0f20-4999-aaa0-550994bcc380"),
+			TenantId:                  pulumi.String("e467b6d8-cf62-4e59-9a87-758ef858aeb6"),
+		},
+		PublicNetworkAccess:           pulumi.String(sql.ServerNetworkAccessFlagDisabled),
+		ResourceGroupName:             args.resourceGroup.Name,
+		RestrictOutboundNetworkAccess: pulumi.String(sql.ServerNetworkAccessFlagDisabled),
+	})
+	if err != nil {
+		return SqlServerReturn{err: err}
+	}
+
+	_, err = sql.NewDatabase(args.ctx, "foxnhound-db", &sql.DatabaseArgs{
+		ResourceGroupName: args.resourceGroup.Name,
+		ServerName:        dbserver.Name,
+		Sku: &sql.SkuArgs{
+			Name: pulumi.String("Basic"),
+			Tier: pulumi.String("Basic"),
+		},
+	})
+	if err != nil {
+		return SqlServerReturn{err: err}
+	}
+
+	// Create a Private Endpoint for the Azure SQL Database
+	privateEndpoint, err := network.NewPrivateEndpoint(args.ctx, "foxnhound-private-endpoint", &network.PrivateEndpointArgs{
+		ResourceGroupName: args.resourceGroup.Name,
+		Subnet: &network.SubnetTypeArgs{
+			Id: subnet.ID(),
+		},
+		PrivateLinkServiceConnections: network.PrivateLinkServiceConnectionArray{
+			&network.PrivateLinkServiceConnectionArgs{
+				Name:                 pulumi.String("sqlPrivateLink"),
+				PrivateLinkServiceId: dbserver.ID(),
+				GroupIds: pulumi.StringArray{
+					pulumi.String("sqlServer"),
+				},
+				RequestMessage: pulumi.String("Please approve my connection"),
+			},
+		},
+	})
+	if err != nil {
+		return SqlServerReturn{err: err}
+	}
+
+	// Create a Private DNS Zone for the SQL Server
+	privateDnsZone, err := network.NewPrivateZone(args.ctx, "foxnhound-private-dns-zone", &network.PrivateZoneArgs{
+		ResourceGroupName: args.resourceGroup.Name,
+		PrivateZoneName:   pulumi.String("foxnhound.privatelink.database.windows.net"),
+		Location:          pulumi.String("Global"),
+	})
+	if err != nil {
+		return SqlServerReturn{err: err}
+	}
+
+	// Create a DNS Zone Group for the Private Endpoint
+	_, err = network.NewPrivateDnsZoneGroup(args.ctx, "foxnhound-dns-zone-group", &network.PrivateDnsZoneGroupArgs{
+		ResourceGroupName:   args.resourceGroup.Name,
+		PrivateEndpointName: privateEndpoint.Name,
+		PrivateDnsZoneConfigs: network.PrivateDnsZoneConfigArray{
+			&network.PrivateDnsZoneConfigArgs{
+				Name:             pulumi.String("sqlDnsZoneConfig"),
+				PrivateDnsZoneId: privateDnsZone.ID(),
+			},
+		},
+	})
+	if err != nil {
+		return SqlServerReturn{err: err}
+	}
+
+	return SqlServerReturn{dbserver: dbserver}
 }
